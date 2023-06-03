24a009da69a9e08de3ff87a618dae65618ada62a
rashifar <rashifar@students.zhaw.ch>
2023-06-03 13:09:14 +0200

    bancomat hinzugefügt

---
+++ backend/src/test/java/ch/zhaw/iwi/devops/Banco/bancomattest.java
@@ -0,0 +1,55 @@
+package ch.zhaw.iwi.devops.Banco;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class bancomattest {
+
+    private bancomat bancomat;
+
+    @BeforeEach
+
+    public void setUp() {
+
+        bancomat = new bancomat(); // Erstellt ein neues Bancomat-Objekt vor jedem Testfall
+
+    }
+    @Test
+
+    public void testInitialBalance() {
+
+        assertEquals(0, bancomat.getBalance()); // Überprüft, ob die anfängliche Kontobalance 0 ist
+    }
+    @Test
+
+    public void testDeposit() {
+
+        bancomat.deposit(100); // Führt eine Einzahlung von 100 durch
+
+        assertEquals(100, bancomat.getBalance()); // Überprüft, ob die Kontobalance nach der Einzahlung 100 ist
+    }
+    @Test
+
+    public void testWithdrawSufficientFunds() {
+
+        bancomat.deposit(100); // Führt eine Einzahlung von 100 durch
+
+        bancomat.withdraw(50); // Führt eine Abhebung von 50 durch
+
+        assertEquals(50, bancomat.getBalance()); // Überprüft, ob die Kontobalance nach der Abhebung 50 ist
+    }
+    @Test
+
+    public void testWithdrawInsufficientFunds() {
+
+        bancomat.deposit(100); // Führt eine Einzahlung von 100 durch
+
+        bancomat.withdraw(150); // Führt eine Abhebung von 150 durch (mehr als der Kontostand)
+
+        assertEquals(100, bancomat.getBalance()); // Überprüft, ob die Kontobalance unverändert bleibt (keine Abhebung erfolgt)
+
+    }
+}
